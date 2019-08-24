28a44b08b9925088ba8df2ec225f0da9fbdfa681
Alexey Boriskin <alexei.boriskin@gmail.com>
2019-08-24 14:09:16 -0500

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/com/alexeiboriskin/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.alexeiboriskin.repository;
+
+import com.alexeiboriskin.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ src/main/java/com/alexeiboriskin/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.alexeiboriskin.service.dto;
